54947d24b204e0eb7e9e3bb985542747e78484f2
fiction13 <fiction13@yandex.ru>
2021-09-23 20:22:49 +0300

    Initial

---
+++ jbzoooverride.php
@@ -0,0 +1,166 @@
+<?php
+/*
+ * @package   System - JBZooOverride
+ * @version   1.0.0
+ * @author    Dmitriy Vasyukov - https://fictionlabs.ru
+ * @copyright Copyright (c) 2021 Fictionlabs. All rights reserved.
+ * @license   GNU/GPL license: http://www.gnu.org/copyleft/gpl.html
+ * @link      https://fictionlabs.ru/
+ */
+
+// no direct access
+defined('_JEXEC') or die('Restricted access');
+
+use Joomla\CMS\Component\ComponentHelper;
+use Joomla\CMS\Plugin\CMSPlugin;
+use Joomla\CMS\Filesystem\File;
+
+class plgSystemJBZooOverride extends CMSPlugin {
+
+	public $app;
+	public $zoo;
+
+	/**
+	 * onAfterInitialise handler
+	 *
+	 * Adds ZOO event listeners
+	 *
+	 * @access public
+	 * @return null
+	 */
+	public function onAfterInitialise() {
+
+		// make sure ZOO exists
+		if (!ComponentHelper::getComponent('com_zoo', true)->enabled) {
+			return;
+		}
+
+		// load ZOO config
+		if (!File::exists(JPATH_ADMINISTRATOR.'/components/com_zoo/config.php') || !ComponentHelper::getComponent('com_zoo', true)->enabled) {
+			return;
+		}
+        
+		require_once(JPATH_ADMINISTRATOR.'/components/com_zoo/config.php');
+
+		// make sure App class exists
+		if (!class_exists('App')) {
+			return;
+		}
+
+		// Get the ZOO App instance
+		$this->zoo = App::getInstance('zoo');
+
+        $this->zoo->event->dispatcher->connect('application:init', array($this, 'initOverride'));
+	}
+
+	/**
+     * Init all
+     * @return void
+     */
+
+	public function initOverride() {
+        $this->_initTables();
+        $this->_initPaths();
+	}
+
+	/**
+     * Add directory path
+     */
+    private function _initPaths()
+    {	
+    	$this->_addPath('jbapp:framework-override', 'jbzoo');
+    	$this->_addPath('jbapp:assets', 'jbassets');
+        $this->_addPath('jbassets:zoo', 'assets');
+        $this->_addPath('jbapp:config', 'jbconfig');
+        $this->_addPath('jbzoo:elements', 'jbelements');
+        $this->_addPath('jbapp:cart-elements', 'cart-elements');
+        $this->_addPath('jbapp:types', 'jbtypes');
+
+        $this->_addPath('jbzoo:helpers', 'helpers');
+        $this->_addPath('jbzoo:helpers-std', 'helpers');
+        $this->_addPath('helpers:fields', 'fields');
+
+        $this->_addPath('jbzoo:tables', 'tables');
+        $this->_addPath('jbzoo:classes-std', 'classes');
+        $this->_addPath('jbzoo:render', 'renderer');
+        $this->_addPath('jbzoo:views', 'jbviews');
+        $this->_addPath('jbapp:config', 'jbxml');
+        $this->_addPath('jbviews:', 'partials');
+        $this->_addPath('jbapp:joomla/elements', 'fields');
+        $this->_addPath('jbapp:templates', 'jbtmpl');
+
+        $this->_addPath('modules:mod_jbzoo_search', 'mod_jbzoo_search');
+        $this->_addPath('modules:mod_jbzoo_props', 'mod_jbzoo_props');
+        $this->_addPath('modules:mod_jbzoo_basket', 'mod_jbzoo_basket');
+        $this->_addPath('modules:mod_jbzoo_category', 'mod_jbzoo_category');
+        $this->_addPath('modules:mod_jbzoo_item', 'mod_jbzoo_item');
+        $this->_addPath('modules:mod_jbzoo_currency', 'mod_jbzoo_currency');
+
+        $this->_addPath('plugins:/system/jbzoo', 'plugin_jbzoo');
+
+        // Add new elements folder
+
+        $this->_addPath('jbzoo:zoo-elements', 'elements');
+
+        // Add controllers
+
+        if ($this->app->isClient('site')) {
+            $this->_addPath('jbzoo:controllers', 'controllers');
+        }
+
+        // Paths
+
+        $frameworkPath = JPATH_SITE . '/media/zoo/applications/jbuniversal/framework-override';
+        $clsPath = $frameworkPath . '/classes';
+        $modPath = $frameworkPath . '/models';
+        $filPath = $frameworkPath . '/render';
+        $evtPath = $frameworkPath . '/events';
+
+        // Add class list
+
+        $classList = [
+            // Models example
+
+            // 'JBModelCustom'          => $modPath . '/jbmodel.custom.php',
+
+            // Filter example
+
+            // 'JBFilterElementCustom'  => $filPath . '/element.custom.php',
+
+            // Classes example
+
+            // 'JBClass'                => $clsPath . '/jbcustom.php',
+        ];
+
+        foreach ($classList as $className => $path) {
+            JLoader::register($className, $path, true);
+        }
+
+        // jbdump($this->zoo->path->paths('elements:'), 0);
+    }
+
+    /**
+     * Add directory path
+     */
+    private function _initTables()
+    {
+        // Define your tables
+        // define('ZOO_TABLE_CUSTOM', '#__zoo_custom');
+    }
+
+	/**
+     * Register new path in system
+     * @param string $path
+     * @param string $pathName
+     * @return mixed
+     */
+    private function _addPath($path, $pathName)
+    {
+        if ($fullPath = $this->zoo->path->path($path)) {
+            return $this->zoo->path->register($fullPath, $pathName);
+        }
+
+        return null;
+    }
+
+}
---
+++ script.php
@@ -0,0 +1,26 @@
+<?php
+/*
+ * @package   System - JBZooOverride
+ * @version   1.0.0
+ * @author    Dmitriy Vasyukov - https://fictionlabs.ru
+ * @copyright Copyright (c) 2021 Fictionlabs. All rights reserved.
+ * @license   GNU/GPL license: http://www.gnu.org/copyleft/gpl.html
+ * @link      https://fictionlabs.ru/
+ */
+
+// no direct access
+defined( '_JEXEC' ) or die( 'Restricted access' );
+
+use Joomla\CMS\Filesystem\Folder;
+
+class plgSystemJBZooOverrideInstallerScript
+{	
+	// Post Install Function
+
+	function postflight($type, $parent)
+	{
+		// Create overrider folder
+
+		Folder::create(JPATH_ROOT.'/media/zoo/applications/jbuniversal/framework-override');
+	}
+}
